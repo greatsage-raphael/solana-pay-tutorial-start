ba79764824a30ee7249252a658eedc5147c58cce
Lubwama Collins <56563537+greatsage-raphael@users.noreply.github.com>
2022-08-30 19:37:40 +0300

    Create addresses.ts

---
+++ lib/addresses.ts
@@ -0,0 +1,5 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+//copy your wallet addresss and paste it where the ... is
+export const shopAddress = new PublicKey('...');
